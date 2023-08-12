45ec2a12814ba0d7ff71d8d5d59510f86f3c0598
ahmadrosid <alahmadrosid@gmail.com>
2023-08-12 20:55:15 +0700

    feat: add format table script

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# epl-fixtures
+# EPL Fixtures
+
+# Fixtures
+
+<!-- START_TABLE -->
+| Team 1 | Team 1 Score | Team 2 | Team 2 Score |
+|--------|--------------|--------|--------------|
+| Burnley | 0 | Manchester City | 3 |
+| Arsenal | 2 | Nottingham Forest | 0 |
+| Bournemouth | 0 | West Ham United | 0 |
+| Brighton & Hove Albion | 0 | Luton Town | 0 |
+| Everton | 0 | Fulham | 0 |
+| Sheffield United | 0 | Crystal Palace | 0 |
+| Newcastle United | N/A | Aston Villa | N/A |
+| Brentford | N/A | Tottenham Hotspur | N/A |
+| Chelsea | N/A | Liverpool | N/A |
+| Manchester United | N/A | Wolverhampton Wanderers | N/A |
+<!-- END_TABLE -->
---
+++ format-table.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Check if jq is installed
+if ! command -v jq &> /dev/null
+then
+    echo "jq could not be found. Please install it."
+    exit
+fi
+
+# Extract table data into a temporary file
+cat fixtures.json | jq -r '
+.content[] |
+[
+    .teams[0].team.name,
+    (if .teams[0].score then .teams[0].score else "N/A" end),
+    .teams[1].team.name,
+    (if .teams[1].score then .teams[1].score else "N/A" end)
+] |
+join(" | ")
+' | awk 'BEGIN {print "| Team 1 | Team 1 Score | Team 2 | Team 2 Score |"; print "|--------|--------------|--------|--------------|";} {print "| "$0" |";}' > /tmp/table_data.txt
+
+
+# Delete content between the markers
+sed -i '' '/<!-- START_TABLE -->/,/<!-- END_TABLE -->/{//!d;}' README.md
+
+# Insert new table data after the start marker
+sed -i '' -e '/<!-- START_TABLE -->/r /tmp/table_data.txt' README.md
+
+# Remove the temporary file
+rm /tmp/table_data.txt
